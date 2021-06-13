a7ee609face63495a97c931e2978adff6ae5ef99
Nimesh Kavinda <44240093+nimeshkavinda@users.noreply.github.com>
2021-06-13 11:03:53 +0530

    fix syntax

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -26,7 +26,8 @@ export default function Jumbotron() {
         setIcon(data.current.weather[0].icon);
         setDescription(data.current.weather[0].description);
         setBackground();
-        console.log(data.current.weather[0].description);
+        console.log(description);
+        console.log(overlay);
       })
       .catch((err) => {
         console.error(err);
@@ -42,47 +43,47 @@ export default function Jumbotron() {
 
   const setBackground = () => {
     switch (description) {
-      case description === "clear sky":
+      case "clear sky":
         setOverlay(
           "https://images.unsplash.com/photo-1558418294-9da149757efe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80"
         );
         break;
-      case description === "few clouds":
+      case "few clouds":
         setOverlay(
           "https://images.unsplash.com/photo-1517685352821-92cf88aee5a5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80"
         );
         break;
-      case description === "scattered clouds":
+      case "scattered clouds":
         setOverlay(
           "https://images.unsplash.com/photo-1511747779856-fd751a79aa22?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
         );
         break;
-      case description === "broken clouds":
+      case "broken clouds":
         setOverlay(
-          "https://images.unsplash.com/photo-1601992664212-8f591f72733b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
+          "https://images.unsplash.com/photo-1606158207522-d9eb6de3ee87?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
         );
         break;
-      case description === "shower rain":
+      case "shower rain":
         setOverlay(
           "https://images.unsplash.com/photo-1494007485290-ce668e189d92?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
         );
         break;
-      case description === "rain":
+      case "rain":
         setOverlay(
           "https://images.unsplash.com/photo-1534274988757-a28bf1a57c17?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80"
         );
         break;
-      case description === "thunderstorm":
+      case "thunderstorm":
         setOverlay(
           "https://images.unsplash.com/photo-1605727216801-e27ce1d0cc28?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1051&q=80"
         );
         break;
-      case description === "snow":
+      case "snow":
         setOverlay(
           "https://images.unsplash.com/photo-1542601098-8fc114e148e2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
         );
         break;
-      case description === "mist":
+      case "mist":
         setOverlay(
           "https://images.unsplash.com/photo-1585508889431-a1d0d9c5a324?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80"
         );
